5e22cbf69212807215f4b78153a7291fcd779d6b
Anusha-Bichal <anushabichal06@gmail.com>
2024-01-31 17:15:13 -0600

    init new examples

---
+++ class/main.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+//javascript is a dynamic typed language
+var myFirstVariable = "hello world";
+console.log(myFirstVariable); //prints the variable
+
+myFirstVariable = 5; //reassigned to a number
+console.log(myFirstVariable);
