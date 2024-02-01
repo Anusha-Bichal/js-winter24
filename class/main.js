21df32823fd1388d68334070ce6501da82f2e5d0
Anusha-Bichal <anushabichal06@gmail.com>
2024-02-01 17:18:59 -0600

    new code

--- class/main.js
+++ class/main.js
@@ -1,7 +1,20 @@
-console.log("Hello World");
+//console.log("Hello World");
 //javascript is a dynamic typed language
 var myFirstVariable = "hello world";
-console.log(myFirstVariable); //prints the variable
+//console.log(myFirstVariable); //prints the variable
 
 myFirstVariable = 5; //reassigned to a number
-console.log(myFirstVariable);
+//console.log(myFirstVariable);
+var myName = "Anu";
+var age = 11;
+var active = "true"; // if it is inside the quotes it is string if not it is a boolean
+var membership = "undefined"; // variable type is undefined
+var data_created = null;
+console.log(typeof membership, membership);
+membership = "gold";
+console.log(typeof membership, membership);
+
+let x = 5; // let allows to assign
+const y = 10; // const does not allow to re-assign
+
+//use "let" if you expect the variable to change else use const
